5b4347fbe2d997fe7aff4052fcf44bd47fab939a
axeloh <axel.harstad@gmail.com>
2018-03-19 19:06:52 +0100

    ...

--- src/core/QueryFactory.java
+++ src/core/QueryFactory.java
@@ -25,16 +25,23 @@ public class QueryFactory {
 	public void setApparat(Apparat apparat) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		String sql = String.format("insert into Apparat values (%s, %s)", apparat.getNavn(), apparat.getBeskrivelse());
 		statement.executeUpdate(sql);
-
 	}
 	
-	public static PreparedStatement setOvelseUtenApparat(OvelseUtenApparat ovelse) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-		PreparedStatement stmt = conn.prepareStatement("insert into Ovelse values (?, ?, ?)"); 
-		stmt.setString(0, ovelse.getNavn());
-		stmt.setString(1, ovelse.getOvelsegruppe().getNavn());
-		stmt.setString(2, ovelse.getBeskrivelse());
-		return stmt;
+//	public static PreparedStatement setOvelseUtenApparat(OvelseUtenApparat ovelse) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+//		PreparedStatement stmt = conn.prepareStatement("insert into Ovelse values (?, ?, ?)"); 
+//		stmt.setString(0, ovelse.getNavn());
+//		stmt.setString(1, ovelse.getOvelsegruppe().getNavn());
+//		stmt.setString(2, ovelse.getBeskrivelse());
+//		return stmt;
+//	}
+	
+	public void setOvelseUtenApparat(OvelseUtenApparat ovelse) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		String sql1 = String.format("insert into Ovelse values (%s, %s)", ovelse.getNavn(), ovelse.getOvelsegruppe());
+		String sql2 = String.format("insert into OvelseUtenApparat values (%s, %s)", ovelse.getNavn(), ovelse.getBeskrivelse());
+		statement.executeUpdate(sql1);
+		statement.executeUpdate(sql2);
 	}
+	
 	
 	public static PreparedStatement setOvelseMedApparat(OvelseMedApparat ovelse) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		PreparedStatement stmt = conn.prepareStatement("insert into Ovelse values (?, ?, ?)"); 
