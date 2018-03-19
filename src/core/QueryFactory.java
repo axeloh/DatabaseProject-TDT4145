ed76701b39b9fb2aca5e203193afd8363f85bc12
axeloh <axel.harstad@gmail.com>
2018-03-19 19:16:38 +0100

    ...

--- src/core/QueryFactory.java
+++ src/core/QueryFactory.java
@@ -15,25 +15,10 @@ public class QueryFactory {
 		statement = conn.createStatement();
 	}
 	
-//	public static PreparedStatement setApparat(Apparat apparat) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-//		PreparedStatement stmt = conn.prepareStatement("insert into Apparat values (?, ?) ");
-//		stmt.setString(0, apparat.getNavn());
-//		stmt.setString(1, apparat.getBeskrivelse());
-//		return stmt;
-//	}
-	
 	public void setApparat(Apparat apparat) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		String sql = String.format("insert into Apparat values (%s, %s)", apparat.getNavn(), apparat.getBeskrivelse());
 		statement.executeUpdate(sql);
 	}
-	
-//	public static PreparedStatement setOvelseUtenApparat(OvelseUtenApparat ovelse) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-//		PreparedStatement stmt = conn.prepareStatement("insert into Ovelse values (?, ?, ?)"); 
-//		stmt.setString(0, ovelse.getNavn());
-//		stmt.setString(1, ovelse.getOvelsegruppe().getNavn());
-//		stmt.setString(2, ovelse.getBeskrivelse());
-//		return stmt;
-//	}
 	
 	public void setOvelseUtenApparat(OvelseUtenApparat ovelse) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		String sql1 = String.format("insert into Ovelse values (%s, %s)", ovelse.getNavn(), ovelse.getOvelsegruppe().getNavn());
