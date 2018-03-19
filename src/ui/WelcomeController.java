7620a24a3dfd271451f90b9d2fa9395940852442
William Kvaale <wgkvaale@hotmail.com>
2018-03-19 21:05:50 +0100

    Frontend init

--- src/ui/WelcomeController.java
+++ src/ui/WelcomeController.java
@@ -1,48 +1,63 @@
 package ui;
 
+import core.Apparat;
+import core.OvelseGruppe;
+import core.OvelseMedApparat;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class WelcomeController {
-	
-	@FXML
-	private Button btnNyOv;
-	@FXML
-	private Button btnRegTren;
-	@FXML
-	private Button btnTidligereTren;
-	
-	@FXML
-	private Button btnSlett;
-	
-	// Refererer til Main
-	private Main main;
-	/**
-	 * @param main Main-instansen som instansierer denne og som inneholder metodene for ï¿½ bytte til de andre viewene.
-	 */
-	public void setMain(Main main) {
-		this.main = main;
-	}
-	
-	@FXML
-	public void handleNyOvelse() {
-		main.showNyOvelse();
-	}
-	
-	@FXML
-	public void handleRegistrerTreningsokt() {
-		main.showAddTrainingSession();
-	}
-	
-	@FXML
-	public void handleTidligereTreningsokter() {
-		main.showVisTrening();
+
+    @FXML
+    private TextField txtFieldNavn;
+
+    @FXML
+    private TextField txtFieldApparat;
+
+    @FXML
+    private TextField txtFieldKilo;
+
+    @FXML
+    private TextField txtFieldReps;
+
+    @FXML
+    private TextField txtFieldBeskrivelse;
+    
+    @FXML
+    private TextField txtFieldOvelse;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Button btnAdd;
+    
+   
+    public void btnLeggTil(ActionEvent event) {
+    	
+    	System.out.println("Legg til pushed");
+    	String Navn = txtFieldNavn.getText();
+    	String ovelsegr = txtFieldOvelse.getText();
 		
-	}
-	
-	@FXML
-	public void handleSlett() {
-		System.out.println("Yo");
-		main.showDeleteExercise();
-	}
+		
+		if (checkBox.isSelected() ){
+			String aparat = txtFieldApparat.getText();
+			Apparat apa = new Apparat(aparat,"penis"); // må hente ut fra DB
+			String Kilo = txtFieldKilo.getText();
+			String Reps = txtFieldReps.getText();
+			OvelseGruppe og = new OvelseGruppe(ovelsegr);
+			OvelseMedApparat oma = new  OvelseMedApparat(Navn,og,apa);
+
+			System.out.println("Med apparat!");
+		}
+		
+		else {
+			String Beskrivelse = txtFieldBeskrivelse.getText();
+		}
+
+    }
+
 }
