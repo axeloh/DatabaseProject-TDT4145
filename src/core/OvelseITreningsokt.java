0e836b4fb05c529122d35a6819b582acdeb408f3
Frikk Hald Andersen <frikkhaldandersen@dhcp-10-22-10-90.wlan.ntnu.no>
2018-03-19 18:48:26 +0100

    Fix

--- src/core/OvelseITreningsokt.java
+++ src/core/OvelseITreningsokt.java
@@ -1,5 +1,64 @@
 package core;
 
 public class OvelseITreningsokt {
+	
+	Treningsokt okt ;
+	Ovelse ovelse;
+	int kilo ;
+	int sett;
+	int reps;
+	
+	public OvelseITreningsokt(Treningsokt okt, Ovelse ovelse, int kilo, int sett, int reps) {
+		super();
+		this.okt = okt;
+		this.ovelse = ovelse;
+		this.kilo = kilo;
+		this.sett = sett;
+		this.reps = reps;
+	}
+
+	public Treningsokt getOkt() {
+		return okt;
+	}
+
+	public void setOkt(Treningsokt okt) {
+		this.okt = okt;
+	}
+
+	public Ovelse getOvelse() {
+		return ovelse;
+	}
+
+	public void setOvelse(Ovelse ovelse) {
+		this.ovelse = ovelse;
+	}
+
+	public int getKilo() {
+		return kilo;
+	}
+
+	public void setKilo(int kilo) {
+		this.kilo = kilo;
+	}
+
+	public int getSett() {
+		return sett;
+	}
+
+	public void setSett(int sett) {
+		this.sett = sett;
+	}
+
+	public int getReps() {
+		return reps;
+	}
+
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
+	
+	
+	
+	
 
 }
--- src/core/Treningsokt.java
+++ src/core/Treningsokt.java
@@ -6,6 +6,64 @@ public class Treningsokt {
 	int treningsoktID; 
 	String date; // "yyyy-mm-dd hh-mm-ss"
 	int varighet;
-	int notatID;
+	Notat notat;
+	
+	
+	
+	public Treningsokt(int treningsoktID, String date, int varighet, Notat notat) {
+		super();
+		this.treningsoktID = treningsoktID;
+		this.date = date;
+		this.varighet = varighet;
+		this.notat = notat;
+	}
 
+
+
+	public int getTreningsoktID() {
+		return treningsoktID;
+	}
+
+
+
+	public void setTreningsoktID(int treningsoktID) {
+		this.treningsoktID = treningsoktID;
+	}
+
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+
+	public int getVarighet() {
+		return varighet;
+	}
+
+
+
+	public void setVarighet(int varighet) {
+		this.varighet = varighet;
+	}
+
+
+
+	public Notat getNotat() {
+		return notat;
+	}
+
+
+
+	public void setNotat(Notat notat) {
+		this.notat = notat;
+	}
+	
 }
